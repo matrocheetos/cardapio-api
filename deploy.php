8124f457986ab156039c0dffe812679c7f7165e2
matrocheetos <58711427+matrocheetos@users.noreply.github.com>
2025-04-03 17:20:10 -0300

    inicio deploy

---
+++ deploy.php
@@ -0,0 +1,24 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony.php';
+
+// Config
+
+set('repository', 'https://github.com/matrocheetos/cardapio-api.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('pratofacil.dynv6.net')
+    ->set('provision_user', 'ubuntu')
+    ->set('become', 'root')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/cardapio-api');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
