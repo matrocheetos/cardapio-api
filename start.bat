9280a3d1cc8c88d518370afc269a8dc85dd83c51
matrocheetos <58711427+matrocheetos@users.noreply.github.com>
2025-05-30 02:19:56 -0300

    docker inicial

---
+++ start.bat
@@ -0,0 +1,11 @@
+docker-compose down --remove-orphans
+
+docker-compose build --no-cache
+
+docker-compose up -d
+
+docker-compose exec app composer install
+
+docker-compose exec app php artisan key:generate
+
+docker-compose exec app php artisan migrate
